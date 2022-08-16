0dda6a2d051c9d6b31f90d3a739e5c1d1c1b178b
Steve Bradshaw <steve@pcfive.co.uk>
2022-08-16 10:31:02 +0100

    Loosed reqs

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ setup(
     keywords=('Django', 'Zoho', 'Subscriptions', 'Python 3',),
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'requests==2.22.0',
-        'gcloud==0.18.3',
-        'oauth2client==4.1.3',
-        'requests_toolbelt==0.9.1',
-        'python_jwt==3.2.4',
-        'pycryptodome==3.9.0',
-        'urllib3==1.25.5'
+        'requests>=2.22.0',
+        'gcloud>=0.18.3',
+        'oauth2client>=4.1.3',
+        'requests_toolbelt>0.9.1',
+        'python_jwt>=3.2.4',
+        'pycryptodome>=3.9.0',
+        'urllib3>=1.25.5'
     ]
 )
